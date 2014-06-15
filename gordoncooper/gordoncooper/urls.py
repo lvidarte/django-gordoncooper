331ac33ed66dde276a35659cfd61154307dbffbe
Leonardo Vidarte <lvidarte@gmail.com>
2014-06-15 01:54:01 -0300

    Added view contact

--- gordoncooper/gordoncooper/urls.py
+++ gordoncooper/gordoncooper/urls.py
@@ -16,15 +16,20 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    #(r'^$', TemplateView.as_view(template_name='about.html')),
+
+    url(r'^$', ListView.as_view(),
+        {'type': None}, name='home'),
+    url(r'^downloads$', ListView.as_view(),
+        {'type': 'download'}, name='downloads'),
+    url(r'^diary$', ListView.as_view(),
+        {'type': 'diary'}, name='diary'),
+    url(r'^gallery$', ListView.as_view(),
+        {'type': 'gallery'}, name='gallery'),
+    url(r'^contact$', TemplateView.as_view(template_name='contact.html'),
+        {'menu': 'contact'}, name='contact'),
 
     url(r'^(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})/(?P<slug>[-\w]+)/$',
         PostView.as_view(), name='post-detail'),
-
-    url(r'^downloads$', ListView.as_view(), {'type': 'download'}, name='downloads'),
-    url(r'^agenda$', ListView.as_view(), {'type': 'diary'}, name='diary'),
-    url(r'^galeria$', ListView.as_view(), {'type': 'gallery'}, name='gallery'),
-    url(r'^$', ListView.as_view(), {'type': None}, name='home'),
 
     #(r'^test-bootstrap$', TemplateView.as_view(template_name='test-bootstrap.html')),
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
